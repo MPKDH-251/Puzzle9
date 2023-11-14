fb2f9f04c38a42856f4f5f1a83261d23e7aef70d
Sreepathy <you@example.com>
2023-11-14 20:55:51 +0530

    initial commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function checkAnswer() {
     const userAnswer = document.getElementById("answer").value.toLowerCase();
 
     if (userAnswer === correctAnswer) {
-        alert("Congratulations! You solved the riddle. Clue: jfjfkf");
+        alert("Congratulations! You solved the riddle. Please go to the above stage and find your next clue!! ");
     } else {
         alert("Oops! Try again.");
     }
